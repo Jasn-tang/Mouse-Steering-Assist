1b315ade2bae6740898840961da2fb530b753dee
Jason Tang <jasontang0724@gmail.com>
2026-09-13 11:27:53 +0800

    Update assist.lua

--- extension/lua/mouse-steering/MSA/assist.lua
+++ extension/lua/mouse-steering/MSA/assist.lua
@@ -1,46 +1,48 @@
---Change script settings here--
--------------------------------
-local smoothness = 0.25 -- How smooth will a change be. (number)
-local doffb = false --Do force feedback or not. (true/false)
-local ffbSens = 0.25 -- How much will the 'force' be. (number)
-local scroll = 0.1 --What percent will each scroll change. (number)
-local throttle = 'W' --Key to press for throttle while scrolling mouse wheel. (alphabet)
-local brake = 'S' --Key to press for brake while scrolling mouse wheel. (alphabet)
-local doForce = true --Force pedals to be 100% or not when shift key is pressed. (true/false)
-local altThrottle = ''
-local altBrake = ''
--------------------------------
+local cfg = ac.INIConfig.scriptSettings():mapSection('SETTINGS', {
+    SMOOTHNESS = 0.25,
+    DOFFB = false,
+    FFBSENS = 0.25,
+    SCROLL = 0.1,
+    THROTTLE = 'W',
+    BRAKE = 'S',
+    ALTTHROTTLE = '',
+    ALTBRAKE = '',
+    FORCEDAMOUNT = 1,
+    ONPRESS = 0.33
+})
 
-function ac.isControllerBrakePressed() end --To tell AC shut up bc this function doesn't exist in CSP 0.2.11 or below version.
+if not ac.isControllerBrakePressed() then function ac.isControllerBrakePressed() end end --To tell AC shut up bc this function doesn't exist in CSP 0.2.11 or below version.
 
 local isFirstGas, isFirstBrake = true, true
 local gasFinal, brakeFinal = 0, 0
 local steerFinal = 0
 local wheel, lastFrameScrolled = 0, 0
 
+
 function script.update(dt, deltaX)
     wheel = 0
     if ac.getUI().mouseWheel ~= 0 and lastFrameScrolled + 1 < ac.getSim().frame then lastFrameScrolled, wheel = ac.getSim().frame, ac.getUI().mouseWheel end
+
     --Mouse Steering--
     steerFinal = math.clamp(steerFinal + deltaX, -1, 1)
-    if doffb then steerFinal = math.clamp(steerFinal - ac.getJoypadState().ffb * ffbSens / 100, -1, 1) end
+    if cfg.DOFFB then steerFinal = math.clamp(steerFinal - ac.getJoypadState().ffb * cfg.FFBSENS / 100, -1, 1) end
 
     --Throttle Part--
-    if ac.isControllerGasPressed() or ac.isKeyDown(ac.KeyIndex[throttle]) or ac.isKeyDown(ac.KeyIndex[altThrottle]) then
-        if isFirstGas then gasFinal, isFirstGas = 1 / 3, false end
-        if ac.isKeyDown(ac.KeyIndex.Shift) and doForce then gasFinal = 1 end
-        gasFinal = math.clamp(gasFinal + wheel * scroll, 0, 1)
+    if ac.isControllerGasPressed() or ac.isKeyDown(ac.KeyIndex[cfg.THROTTLE]) or ac.isKeyDown(ac.KeyIndex[cfg.ALTTHROTTLE]) then
+        if isFirstGas then gasFinal, isFirstGas = cfg.ONPRESS, false end
+        if ac.isKeyDown(ac.KeyIndex.Shift) and cfg.FORCEDAMOUNT ~= 0 then gasFinal = cfg.FORCEDAMOUNT end
+        gasFinal = math.clamp(gasFinal + wheel * cfg.SCROLL, 0, 1)
     else gasFinal, isFirstGas = 0, true end
 
     --Brake Part--
-    if ac.isControllerBrakePressed() or ac.isKeyDown(ac.KeyIndex[brake]) or ac.isKeyDown(ac.KeyIndex[altBrake]) then
-        if isFirstBrake then brakeFinal, isFirstBrake = 1 / 3, false end
-        if ac.isKeyDown(ac.KeyIndex.Shift) and doForce then brakeFinal = 1 end
-        brakeFinal = math.clamp(brakeFinal + wheel * scroll, 0, 1)
+    if ac.isControllerBrakePressed() or ac.isKeyDown(ac.KeyIndex[cfg.BRAKE]) or ac.isKeyDown(ac.KeyIndex[cfg.ALTBRAKE]) then
+        if isFirstBrake then brakeFinal, isFirstBrake = cfg.ONPRESS, false end
+        if ac.isKeyDown(ac.KeyIndex.Shift) and cfg.FORCEDAMOUNT ~= 0 then brakeFinal = cfg.FORCEDAMOUNT end
+        brakeFinal = math.clamp(brakeFinal + wheel * cfg.SCROLL, 0, 1)
     else brakeFinal, isFirstBrake = 0, true end
 
     --Output Part-
     ac.getJoypadState().steer = steerFinal
-    ac.getJoypadState().gas = math.lerp(ac.getCar(0).gas, gasFinal, smoothness)
-    ac.getJoypadState().brake = math.lerp(ac.getCar(0).brake, brakeFinal, smoothness)
+    ac.getJoypadState().gas = math.lerp(ac.getCar(0).gas, gasFinal, cfg.SMOOTHNESS)
+    ac.getJoypadState().brake = math.lerp(ac.getCar(0).brake, brakeFinal, cfg.SMOOTHNESS)
 end
